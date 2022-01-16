9f3f99d3a6d2b2713da4760606f8e44a1e7aa32a
Ã^thiagojdb <thiagojdb@tutanota.com>
2022-01-16 19:34:16 -0300

    Rest Testado

--- src/main/java/com/coffeebrewer/cursoaula2/Cursoaula2Application.java
+++ src/main/java/com/coffeebrewer/cursoaula2/Cursoaula2Application.java
@@ -2,12 +2,22 @@ package com.coffeebrewer.cursoaula2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @SpringBootApplication
+@RestController
 public class Cursoaula2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Cursoaula2Application.class, args);
     }
 
+    @RequestMapping("/")
+    String home()
+    {
+        return "Hello World!";
+    }
 }
