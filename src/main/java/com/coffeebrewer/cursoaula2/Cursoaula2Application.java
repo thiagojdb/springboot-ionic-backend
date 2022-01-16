223ca4f7d45d3119b542203f9bd4b99543199019
Ã^thiagojdb <thiagojdb@tutanota.com>
2022-01-16 15:37:54 -0300

    Projeto Cridado

---
+++ src/main/java/com/coffeebrewer/cursoaula2/Cursoaula2Application.java
@@ -0,0 +1,13 @@
+package com.coffeebrewer.cursoaula2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursoaula2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cursoaula2Application.class, args);
+    }
+
+}
