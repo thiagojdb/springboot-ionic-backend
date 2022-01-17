c4721752836905b6f00ca447f963b4f2ff6c61eb
Ã^thiagojdb <thiagojdb@tutanota.com>
2022-01-17 15:21:29 -0300

    Estado e Cidade

--- src/main/java/com/coffeebrewer/cursoaula2/Cursoaula2Application.java
+++ src/main/java/com/coffeebrewer/cursoaula2/Cursoaula2Application.java
@@ -1,8 +1,12 @@
 package com.coffeebrewer.cursoaula2;
 
 import com.coffeebrewer.cursoaula2.domain.Categoria;
+import com.coffeebrewer.cursoaula2.domain.Cidade;
+import com.coffeebrewer.cursoaula2.domain.Estado;
 import com.coffeebrewer.cursoaula2.domain.Produto;
 import com.coffeebrewer.cursoaula2.repositories.CategoriaRepository;
+import com.coffeebrewer.cursoaula2.repositories.CidadeRepository;
+import com.coffeebrewer.cursoaula2.repositories.EstadoRepository;
 import com.coffeebrewer.cursoaula2.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -24,6 +28,12 @@ public class Cursoaula2Application implements CommandLineRunner {
 
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Cursoaula2Application.class, args);
@@ -50,5 +60,19 @@ public class Cursoaula2Application implements CommandLineRunner {
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas ", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
     }
+
 }
--- src/main/java/com/coffeebrewer/cursoaula2/repositories/CidadeRepository.java
+++ src/main/java/com/coffeebrewer/cursoaula2/repositories/CidadeRepository.java
@@ -1,10 +1,10 @@
 package com.coffeebrewer.cursoaula2.repositories;
 
-import com.coffeebrewer.cursoaula2.domain.Produto;
+import com.coffeebrewer.cursoaula2.domain.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
 
 }
