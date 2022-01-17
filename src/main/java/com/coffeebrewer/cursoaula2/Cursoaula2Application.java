f02d63b73313d2942ab98f50a76842e4810afd48
Ã^thiagojdb <thiagojdb@tutanota.com>
2022-01-17 09:33:43 -0300

    Criando operação de instanciaçãoÃÃ

--- src/main/java/com/coffeebrewer/cursoaula2/Cursoaula2Application.java
+++ src/main/java/com/coffeebrewer/cursoaula2/Cursoaula2Application.java
@@ -1,23 +1,36 @@
 package com.coffeebrewer.cursoaula2;
 
+import com.coffeebrewer.cursoaula2.domain.Categoria;
+import com.coffeebrewer.cursoaula2.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @SpringBootApplication
 @RestController
-public class Cursoaula2Application {
+public class Cursoaula2Application implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Cursoaula2Application.class, args);
     }
 
-    @RequestMapping("/")
-    String home()
-    {
-        return "Hello World!";
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
     }
 }
