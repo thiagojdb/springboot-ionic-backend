2b88433940f33cd55a4e28a1f40762f40d209d9a
Ã^thiagojdb <thiagojdb@tutanota.com>
2022-01-20 11:19:48 -0300

    Anotacao

--- src/main/java/com/coffeebrewer/cursoaula2/repositories/ClienteRepository.java
+++ src/main/java/com/coffeebrewer/cursoaula2/repositories/ClienteRepository.java
@@ -3,8 +3,13 @@ package com.coffeebrewer.cursoaula2.repositories;
 import com.coffeebrewer.cursoaula2.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 
 }
--- src/main/java/com/coffeebrewer/cursoaula2/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/coffeebrewer/cursoaula2/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.coffeebrewer.cursoaula2.services.validation;
 
+import com.coffeebrewer.cursoaula2.domain.Cliente;
 import com.coffeebrewer.cursoaula2.domain.enums.TipoCliente;
 import com.coffeebrewer.cursoaula2.dto.ClienteNewDTO;
+import com.coffeebrewer.cursoaula2.repositories.ClienteRepository;
 import com.coffeebrewer.cursoaula2.resources.exception.FieldMessage;
 import com.coffeebrewer.cursoaula2.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,9 @@ import javax.validation.ConstraintValidatorContext;
 
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
 
     @Override
     public void initialize(ClienteInsert ann) {
@@ -26,6 +32,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         }
         if (objDto.getTipo() == TipoCliente.PESSOAJURIDICA.getCod() && !BR.isValidTfn(objDto.getCpfOuCnpj())){
             list.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email","Email já existente"));
+
         }
 
         for (FieldMessage e : list) {
