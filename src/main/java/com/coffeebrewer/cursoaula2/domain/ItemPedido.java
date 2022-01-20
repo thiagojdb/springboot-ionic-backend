088e9179a7a18bd711c0f123caa451cba6724622
Ã^thiagojdb <thiagojdb@tutanota.com>
2022-01-20 15:08:23 -0300

    SubTotal e Total de Itens e ItemPedido

--- src/main/java/com/coffeebrewer/cursoaula2/domain/ItemPedido.java
+++ src/main/java/com/coffeebrewer/cursoaula2/domain/ItemPedido.java
@@ -29,6 +29,11 @@ public class ItemPedido implements Serializable {
         this.quantidade = quantidade;
         this.preco = preco;
     }
+
+    public Double getSubtTotal(){
+        return (preco - desconto) * quantidade;
+    }
+
     @JsonIgnore
     public Pedido getPedido(){
         return id.getPedido();
